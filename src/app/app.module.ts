324a10e1cbf7dea1376532d9125eb61f236dbe02
ADJIANTADABO <dablisco@gmail.com>
2019-08-22 09:50:19 +0000

    fist-angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { PostComponent } from './post/post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AuthentificationComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
